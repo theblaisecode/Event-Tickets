28d8f47178bf6df692c49ce71a381c598b6f1f71
Blaise Nwachukwu <89015653+theblaisecode@users.noreply.github.com>
2025-01-25 09:26:04 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,25 @@ import { Home, CodeOfConduct } from "./pages";
 import HomeLayout from "./Layout/HomeLayout";
 // import { ticketFormAction } from "./components/Form";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeLayout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "code-of-conduct",
-        element: <CodeOfConduct />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <HomeLayout />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "code-of-conduct",
+          element: <CodeOfConduct />,
+        },
+      ],
+    },
+  ],
+  { basename: "/Event-Tickets" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Event-Tickets/",
   plugins: [react()],
+  base: "/Event-Tickets/",
 });
